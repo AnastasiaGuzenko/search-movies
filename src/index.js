8588a2817244bdf916ee56f0f5da019bebcac442
Anastasia <guzenkoanvl@gmail.com>
2022-06-27 11:33:29 +0300

    roters

--- src/index.js
+++ src/index.js
@@ -2,14 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import './index.css'
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
+    {/* <BrowserRouter>
+      
+    </BrowserRouter> */}
   </React.StrictMode>,
   document.getElementById('root')
 );
